af26fb13a7f50b672fb1eace27b5fcc270508e86
dsdjoy4@gmail.com <dsdjoy4@gmail.com>
2022-01-08 16:35:58 +1100

    q1 file generation 80% done.

---
+++ q1.js
@@ -0,0 +1,72 @@
+function generateShortCode(storeId, transactionId) {
+    // Logic goes here
+    const alps = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    /// storeID
+    let storeReps = {};
+
+    let alpsPermutations = [];
+
+    for (a1 of alps) {
+      for (a2 of alps) {
+        alpsPermutations.push(a1 + a2);
+      }
+    }
+
+    for (let i = 1; i <= 200; i++) {
+      storeReps[i] = alpsPermutations[i-1];
+    }
+    ///store id
+
+    ///date
+    const date = ['1', '2', '3', '4', '5', ...alps];
+    const month = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'];
+    const year = [...alps];
+
+    const dateReps = {};
+    for (let i = 1; i <=31; i++) {
+      dateReps[i] = date[i-1];
+    }
+
+    const monthReps = {};
+    for (let i = 1; i <= 12; i++) {
+      monthReps[i] = month[i-1];
+    }
+
+    const yearReps = {};
+    let y = 2009;
+    for (let i = 1; i <= 26; i++) {
+      yearReps[y] = year[i-1];
+      y++;
+    }
+
+    const d = new Date();
+    console.log(d.getDate(), d.getMonth(), d.getFullYear())
+    console.log(yearReps);
+
+
+    ////date
+
+
+    ///transactionid
+    let transactionReps = {};
+    let alpsPermutationsTransaction = [];
+
+    for (a1 of alps) {
+      for (a2 of alps) {
+        for (a3 of alps) {
+          alpsPermutationsTransaction.push(a1+a2+a3);
+        }
+      }
+    }
+
+    for (let i = 1; i <= 10000; i++) {
+      transactionReps[i] = alpsPermutationsTransaction[i-1];
+    }
+    ///transactionid
+
+    console.log(storeReps[storeId], transactionReps[transactionId]);
+
+}
+
+generateShortCode(100,100);
