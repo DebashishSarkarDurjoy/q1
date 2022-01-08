77cddb5311519ef1ec36a5f93316b657558fa7f7
dsdjoy4@gmail.com <dsdjoy4@gmail.com>
2022-01-08 19:17:35 +1100

    Tests added

--- q1.js
+++ q1.js
@@ -1,42 +1,66 @@
+// Logic goes here
+const alps = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+/// storeID
+let storeReps = {};
+
+let alpsPermutations = [];
+
+for (a1 of alps) {
+  for (a2 of alps) {
+    alpsPermutations.push(a1 + a2);
+  }
+}
+
+for (let i = 0; i < 200; i++) {
+  storeReps[i] = alpsPermutations[i];
+}
+let storeReps_switch = {};
+Object.keys(storeReps).forEach((key) => {
+    storeReps_switch[storeReps[key]] = key;
+})
+///store id
+
+///transactionid
+let transactionReps = {};
+let alpsPermutationsTransaction = [];
+
+for (a1 of alps) {
+  for (a2 of alps) {
+    for (a3 of alps) {
+      alpsPermutationsTransaction.push(a1+a2+a3);
+    }
+  }
+}
+
+for (let i = 1; i <= 10000; i++) {
+  transactionReps[i] = alpsPermutationsTransaction[i-1];
+}
+let transactionReps_switch = {};
+Object.keys(transactionReps).forEach((key) => {
+    transactionReps_switch[transactionReps[key]] = key;
+});
+///transactionid
+
 function generateShortCode(storeId, transactionId) {
-    // Logic goes here
-    const alps = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-    /// storeID
-    let storeReps = {};
 
-    let alpsPermutations = [];
+    return storeReps[storeId] + transactionReps[transactionId];
 
-    for (a1 of alps) {
-      for (a2 of alps) {
-        alpsPermutations.push(a1 + a2);
-      }
-    }
+}
 
-    for (let i = 1; i <= 200; i++) {
-      storeReps[i] = alpsPermutations[i-1];
-    }
-    ///store id
+function decodeShortCode(shortCode) {
+  // Logic goes here
+  let shortCode_array = shortCode.split("");
+  let store_id = shortCode.slice(0,2);
+  let transaction_id = shortCode.slice(2, 5);
 
-    ///transactionid
-    let transactionReps = {};
-    let alpsPermutationsTransaction = [];
 
-    for (a1 of alps) {
-      for (a2 of alps) {
-        for (a3 of alps) {
-          alpsPermutationsTransaction.push(a1+a2+a3);
-        }
-      }
-    }
-
-    for (let i = 1; i <= 10000; i++) {
-      transactionReps[i] = alpsPermutationsTransaction[i-1];
-    }
-    ///transactionid
-
-    console.log(storeReps[storeId], transactionReps[transactionId]);
-
+  return {
+    storeId: parseInt(storeReps_switch[store_id]), // store id goes here,
+    shopDate: new Date(), // the date the customer shopped,
+    transactionId: parseInt(transactionReps_switch[transaction_id]) // transaction id goes here
+  };
 }
 
 function generateDate() {
@@ -70,5 +94,14 @@ function generateDate() {
     ////date
 }
 
-generateShortCode(100,100);
-generateDate();
+
+
+var storeIds = [175, 42, 0, 9];
+var transactionIds = [9675, 23, 123, 7];
+
+for (sid of storeIds) {
+  for (tid of transactionIds) {
+    console.log(generateShortCode(sid, tid));
+    console.log(decodeShortCode(generateShortCode(sid, tid)));
+  }
+}
